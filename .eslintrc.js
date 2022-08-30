f8bf5a4928a1b90a155090e7c03510e528aa523f
gurujowa <gurujowa@gmail.com>
2022-08-30 02:30:52 +0000

    interactive ignore

--- .eslintrc.js
+++ .eslintrc.js
@@ -11,7 +11,7 @@ module.exports = {
     sourceType: 'module',
   },
 
-  ignorePatterns: ['frontend/src/**'],
+  ignorePatterns: ['frontend/src/**', 'interactive/**'],
   plugins: ['@typescript-eslint'],
   rules: {},
 }
