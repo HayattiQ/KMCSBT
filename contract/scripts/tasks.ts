6a7a5fa92f7633c767068a1a35e2f5fb12cf3167
gurujowa <gurujowa@gmail.com>
2022-08-31 09:06:55 +0000

    Airdrop task added

--- contract/scripts/tasks.ts
+++ contract/scripts/tasks.ts
@@ -1,70 +1,45 @@
 /* eslint-disable dot-notation */
-import { task, types } from 'hardhat/config'
-import { ethers } from 'ethers'
-import { getContract, getEnvVariable, getProvider } from './helpers'
+import { task } from 'hardhat/config'
+import { getContract, getProvider } from './helpers'
 import fs from 'fs'
-import readline from 'readline'
+import type { KMCSelect } from '../typechain-types'
+const { parse } = require('csv-parse/sync')
 
-task('checksum', 'Change address to checksum address')
-  .addParam('address', 'wallet address')
-  .setAction(async (taskArgs, hre) => {
-    console.log(ethers.utils.getAddress(taskArgs.address))
-  })
-
-task('pushWL', 'Push WhiteList from JSON file')
+task('airdrop', 'Push WhiteList from JSON file')
   .addOptionalParam(
     'filename',
     'WhiteList txt file name',
-    './scripts/whitelist_import.txt'
+    './scripts/KMCS_airdrop.csv'
   )
   .setAction(async (taskArgs, hre) => {
-    const whitelist: string[] = []
-
-    const rl = readline.createInterface({
-      input: fs.createReadStream(taskArgs.filename, { encoding: 'utf8' }),
-      crlfDelay: Infinity,
-    })
-
-    for await (const line of rl) {
-      if (!ethers.utils.isAddress(line)) throw Error(line + 'is not valid.')
-      whitelist.push(line)
-    }
-
-    const contract = await getContract(
-      getEnvVariable('CONTRACT_NAME'),
+    const contract = (await getContract(
+      'KMCSelect',
       hre,
       getProvider(hre)
-    )
-    const transactionResponse = await contract['pushMultiWL'](whitelist, {
-      gasLimit: 14900000,
+    )) as KMCSelect
+    const records = parse(fs.readFileSync('./scripts/KMCS_airdrop.csv'), {
+      columns: true,
     })
-    console.log(`Transaction Hash: ${transactionResponse.hash}`)
-  })
-
-task('snapshot', 'Take Snapshot NFT')
-  .addOptionalParam(
-    'filename',
-    'White txt file name',
-    './scripts/snapshot.csv',
-    types.string
-  )
-  .addOptionalParam('start', 'Start ID', 1, types.int)
-  .setAction(async function (taskArguments, hre) {
-    const contract = await getContract(
-      getEnvVariable('CONTRACT_NAME'),
-      hre,
-      getProvider(hre)
-    )
-    const totalSupply: number = Number(await contract['totalSupply']())
-    console.log(`totalSupply: ${totalSupply}`)
-    if (fs.existsSync(taskArguments.filename)) {
-      fs.truncateSync(taskArguments.filename)
+    for (const ad of records.filter(
+      (e: { superrare: number }) => e.superrare >= 1
+    )) {
+      let loop = true
+      let count = 0
+      while (loop && count < 3) {
+        try {
+          const tx = await contract.mint(
+            ad.HolderAddress,
+            2,
+            ad.superrare,
+            '0x00'
+          )
+          console.log(tx.hash)
+          await tx.wait()
+          loop = false
+        } catch (e) {
+          count++
+          console.log('error we send again.address = ' + ad.HolderAddress)
+        }
+      }
     }
-    for (let i = taskArguments.start; i <= totalSupply; i++) {
-      const ownerOf = await contract['ownerOf'](i)
-      console.log(`ID:${i} owner:${ownerOf}`)
-      fs.appendFileSync(taskArguments.filename, [i, ownerOf].join(',') + '\n')
-    }
-
-    console.log('done')
   })
